eb0c684b24e7c44a8d5e956f5541c00e2d55c13a
ahanv <hanvey126@gmail.com>
2022-09-06 10:45:35 -0400

    Removed commented code

--- UserService/index.ts
+++ UserService/index.ts
@@ -4,10 +4,6 @@ import routes from './routes';
 const port = 3005;
 const app = express();
 
-/* app.get('/', (req, res) => {
-    res.send('Overwatch is running!');
-}) */
-
 app.use('/api/', routes);
 
 app.listen(port, () => {
