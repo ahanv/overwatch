22af561ade9b3583304371165dd8a039a4fc3ff2
ahanv <hanvey126@gmail.com>
2022-09-06 10:11:59 -0400

    Updates to user endpoint

--- UserService/services/user.ts
+++ UserService/services/user.ts
@@ -1,23 +1,16 @@
-var AWS = require("aws-sdk");
-import { DynamoDBClient, GetItemCommand, GetItemCommandInput } from "@aws-sdk/client-dynamodb";
+import AWS from 'aws-sdk';
 
-export const readUser = async (): Promise<{ id: any; type: any; name: any; }> => {
-    console.log('****TEST 3****');
+export const readUser = async () => {
     AWS.config.update({
     region: "local",
-    endpoint: "http://localhost:8000"
     });
 
-    let userPayload: {Item: { id: any; type: any; name: any; }} = {"Item": { "id": "999", "type": "init", "name": "Initial Rec" }};
-
-    console.log('****TEST 4****');
-    userPayload = await run();
-    console.log('*****HEY*******', userPayload);
-    return userPayload.Item;
+    const userPayload = await run();
+    return userPayload;
 };
 
-export const run = async (): Promise<{Item: { id: any; type: any; name: any; }}> => {
-    const docClient = new AWS.DynamoDB.DocumentClient()
+export const run = async () => {
+    const docClient = new AWS.DynamoDB.DocumentClient({region: "local", endpoint: "http://localhost:8000"})
     const table = "Users";
     const id = 1;
     const params = {
@@ -26,20 +19,9 @@ export const run = async (): Promise<{Item: { id: any; type: any; name: any; }}>
             "id": id
         }
     };
-    console.log('******TEST 4.2*******');
     try {
-        await docClient.get(params, function(err: any, data: any) {
-            if (err) {
-                console.log('********TEST 4.2.1*******')
-                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-            } else {
-                console.log('****TEST 5****');
-                console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-                return data;
-            }
-        })
+        return (await docClient.get(params).promise()).Item
     } catch (err) {
-        console.log("********TEST 4.3*******")
-        return {Item: { id: "error", type: "error", name: "error", }};
+        console.error(JSON.stringify(err));
     }
 };
